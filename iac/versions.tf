d84cb7987e9e39f2afd8da962ed00167fe0753d2
Kavibritto Chalaman <kavibritto007@gmail.com>
2025-09-04 13:12:58 +0530

    remote backend added

--- iac/versions.tf
+++ iac/versions.tf
@@ -5,6 +5,15 @@ terraform {
       version = "4.42.0"
     }
   }
+
+  backend "azurerm" {
+    tenant_id = "5560a310-6249-441c-a7a8-22324a8b8ce4"
+    resource_group_name  = "rg-tfstates"
+    storage_account_name = "st24951tfstatekavi"   # or your SA from step 1
+    container_name       = "tfstate"
+    key                  = "platform/infra.tfstate"
+
+  }
 }
 
 provider "azurerm" {
@@ -14,3 +23,4 @@ provider "azurerm" {
 
   }
 }
+
